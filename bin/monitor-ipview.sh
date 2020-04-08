f34a24ad820bf142e0425c993167a0af4ed098db
kehao <kehao1158115@outlook.com>
2020-04-08 13:17:05 +0800

    monitor-ipview.sh刷新环境变量

--- bin/monitor-ipview.sh
+++ bin/monitor-ipview.sh
@@ -2,10 +2,13 @@
 
 # */5 * * * * /home/kehao/ipview/bin/monitor-ipview.sh
 
-cmd=`netstat -anp | grep -w 8091 | grep -w LISTEN | wc -l`
+TIME=$(date +'%Y%m%d%H%M')
+cmd=`netstat -anp | grep -w 8090 | grep -w LISTEN | wc -l`
 if [ "$cmd" -eq "0" ];then
-    sh /home/kehao/ipview/bin/start-ipview.sh
-    exit
+    echo $TIME shutdown.  >> /tmp/ipview.monitor
+    source /home/kehao/.bash_profile
+    . /home/kehao/ipview/bin/start-ipview.sh
 else
+    echo $TIME running.  >> /tmp/ipview.monitor
     exit 0
 fi
